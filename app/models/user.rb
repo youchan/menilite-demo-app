7b9606104110f6998d7de9ec033eeb5fdedc59d8
youchan <youchan01@gmail.com>
2016-09-07 04:24:58 +0900

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
+unless RUBY_ENGINE == 'opal'
+  require 'bcrypt'
+end
+
 class User < Menilite::Model
   field :name
   field :password
+
+  action :signup, on_create: true do |password|
+    self.password = BCrypt::Password.create(password)
+    self.save
+  end
 end
--- app/signup.rb
+++ app/signup.rb
@@ -7,11 +7,18 @@ class AppView
   include Hyalite::Component
   include Hyalite::Component::ShortHand
 
+  def signup
+    user = User.new(name: @refs[:name].value)
+    user.signup(@refs[:password].value) do
+      `window.location = '/'`
+    end
+  end
+
   def render
     div({class: 'signup-view'},
       p(nil, input({type: 'text', ref: 'name'})),
       p(nil, input({type: 'text', ref: 'password'})),
-      p(nil, button({}, 'Signup'))
+      p(nil, button({onClick: self.method(:signup)}, 'Signup'))
     )
   end
 end
