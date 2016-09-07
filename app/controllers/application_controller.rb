d8e7f64ed6d17cfd6a426e3cc88a9a1016c09321
youchan <youchan01@gmail.com>
2016-09-07 11:58:09 +0900

    add user_privilege

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < Menilite::Controller
+  before_action(exclude: ['ApplicationController#login', 'User#auth']) do
+    user = User[session[:user_id]]
+    if user
+      Menilite::PrivilegeService.current.privileges << UserPrivilege.new(user)
+    else
+      raise 'auth error'
+    end
+  end
+
   action :login do |username, password|
     user = User.find(name: username)
     if user && user.auth(password)
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,7 @@
 class Todo < Menilite::Model
   field :description
   field :done, :boolean
+  field :user, :reference
+
+  permit :user_privilege
 end
